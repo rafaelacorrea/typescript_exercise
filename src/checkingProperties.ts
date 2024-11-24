1551f248f406463afbc023ad9093be37b2c2455a
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2024-11-24 19:51:54 -0300

    feat: :sparkles: Checking properties

---
+++ src/checkingProperties.ts
@@ -0,0 +1,105 @@
+/*
+O que está errado?
+
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+type Person = User | Admin;
+
+const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut.'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver.'
+    }
+];
+
+function logPerson(person: Person) {
+    let additionalInformation: string;
+    if (person.role) {
+        additionalInformation = person.role;
+    } else {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
+*/
+
+/*R: Na verificação da condição, temos que validar se existe uma propriedade 
+chamada 'role' dentro do parâmetro person, já que ela não está diretamente
+iniciada detro da const persons.*/
+
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+type Person = User | Admin;
+
+const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+function logPerson(person: Person) {
+    let additionalInformation: string;
+    if ('role' in person) {
+        additionalInformation = person.role;
+    } else {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
