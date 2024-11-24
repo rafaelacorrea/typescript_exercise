54648cddb91658e2a53be47b8def9138dfa091f4
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2024-11-24 17:04:03 -0300

    feat: :sparkles: Type in arrays

---
+++ src/typeInArray.ts
@@ -0,0 +1,56 @@
+/*
+O que está errado?
+
+const integer = 6;
+const float = 6.66;
+const hex = 0xf00d;
+const binary = 0b1010011010;
+const octal = 0o744;
+const negZero = -0;
+const actuallyNumber = NaN;
+const largestNumber = Number.MAX_VALUE;
+const mostBiglyNumber = Infinity;
+
+const members: any[] = [
+  integer,
+  float,
+  hex,
+  binary,
+  octal,
+  negZero,
+  actuallyNumber,
+  largestNumber,
+  mostBiglyNumber
+];
+
+members[0] = '12345';
+
+console.log(members);
+*/
+
+//R: todas as const abaixo são do tipo number
+const integer: number = 6;
+const float: number = 6.66;
+const hex: number = 0xf00d;
+const binary: number = 0b1010011010;
+const octal: number = 0o744;
+const negZero: number = -0;
+const actuallyNumber: number = NaN;
+const largestNumber: number = Number.MAX_VALUE;
+const mostBiglyNumber: number = Infinity;
+
+const members: number[] = [
+  integer,
+  float,
+  hex,
+  binary,
+  octal,
+  negZero,
+  actuallyNumber,
+  largestNumber,
+  mostBiglyNumber
+];
+//A const que recebe o array é do tipo number e não string
+members[0] = 12345;
+
+console.log(members);
