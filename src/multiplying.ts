87e43d5b8b97285425fa2f8fb61772dc998b9fe4
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2024-11-24 15:26:25 -0300

    feat: :sparkles: multiplying

---
+++ src/multiplying.ts
@@ -0,0 +1,13 @@
+/*
+O que está errado?
+let pi = '3.14159';
+let tau = pi * 2;
+
+console.log(`${tau} is ${pi} times two.`);
+*/
+
+//R: não conseguimos fozer operações matemáticas usando o tipo string
+let pi = 3.14159;
+let tau = pi * 2;
+
+console.log(`${tau} is ${pi} times two.`);
