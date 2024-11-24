119ee0dd47c3183dc237ded8e261046df429b572
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2024-11-24 16:25:28 -0300

    feat: :sparkles: typing variables

---
+++ dist/assigningValues.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+O que está errado?
+let isDouglas: boolean
+
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+*/
+//R: um valor precisa ser atribuído para a variável
+let isDouglas = false;
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`);
---
+++ src/assigningValues.ts
@@ -0,0 +1,12 @@
+/*
+O que está errado?
+let isDouglas: boolean
+
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+*/
+
+//R: um valor precisa ser atribuído para a variável
+
+let isDouglas: boolean = false
+
+console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
